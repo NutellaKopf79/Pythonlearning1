2c79e0c1ab3a2ff61ed9ca97672e354ce9a2a09b
NutellaKopf79 <valentin.hoffmann595@icloud.com>
2024-01-23 19:01:12 +0100

    Lotterie nachmacher

--- learn.py
+++ learn.py
@@ -1,9 +1,10 @@
-print("Welcome, what are you going to buy today?")
+number1 = int(input("Bitte wähle eine Zahl zwischen 1 und 100: "))
+number2 = int(input("Bitte wähle eine Zahl zwischen 1 und 100: "))
+number3 = int(input("Bitte wähle eine Zahl zwischen 1 und 100: "))
+number4 = int(input("Bitte wähle eine Zahl zwischen 1 und 100: "))
+number5 = int(input("Bitte wähle eine Zahl zwischen 1 und 100: "))
 
-print("This is our list!")
-print("Pizza")
-
-print("test")
-
-print("antother")
-
+if number1 == 1 and number2 == 2 and number3 == 3 and number4 == 4 and number5 == 69:
+	print("Du kleines Monke hast gewonnen")
+else:
+	print("Bruda...Du hast verloren")
